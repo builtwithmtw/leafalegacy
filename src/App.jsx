ad7ae8aeffedd73938dac1496b79c9b6de3c78f9
MTW <89306297+iammtw-dev@users.noreply.github.com>
2025-07-09 22:31:03 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ForestsPage from "./pages/forests";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import ForestDetail from "./pages/ForestDetail";
 import ThankYouPage from "./pages/thankyou";
 
